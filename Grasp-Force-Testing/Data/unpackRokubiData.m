1abb2107ff10df1ec4cded423b568a91b9920211
Alexander Ambrose <38139305+aambrose3@users.noreply.github.com>
2026-04-14 01:14:24 -0500

    Reinit

--- Grasp-Force-Testing/Data/unpackRokubiData.m
+++ Grasp-Force-Testing/Data/unpackRokubiData.m
@@ -14,16 +14,13 @@
 clear all; close all; clc;
 
 %% Import Rokubi Data
-RData.t1 = readtable('T1.csv', 'FileType', 'text');
-RData.t2 = readtable('T2.csv', 'FileType', 'text');
-RData.t3 = readtable('T3.csv', 'FileType', 'text');
-RData.t4 = readtable('T4.csv', 'FileType', 'text');
-RData.t5 = readtable('T5.csv', 'FileType', 'text');
-RData.t6 = readtable('T6.csv', 'FileType', 'text');
-RData.t7 = readtable('T7.csv', 'FileType', 'text');
-RData.t8 = readtable('T8.csv', 'FileType', 'text');
-RData.t9 = readtable('T9.csv', 'FileType', 'text');
-RData.t10 = readtable('T10.csv', 'FileType', 'text');
+RData.t1 = readtable('Grasp_Force_Testing_Rokubi\60x75\T1.csv', 'FileType', 'text');
+RData.t2 = readtable('Grasp_Force_Testing_Rokubi\60x75\T2.csv', 'FileType', 'text');
+RData.t3 = readtable('Grasp_Force_Testing_Rokubi\60x75\T3.csv', 'FileType', 'text');
+RData.t4 = readtable('Grasp_Force_Testing_Rokubi\60x75\T4.csv', 'FileType', 'text');
+RData.t5 = readtable('Grasp_Force_Testing_Rokubi\60x75\T5.csv', 'FileType', 'text');
+RData.t6 = readtable('Grasp_Force_Testing_Rokubi\60x75\T6.csv', 'FileType', 'text');
+
 
 fieldNames = fieldnames(RData);
 kk = 1;
@@ -31,21 +28,16 @@ for ii = 1:numel(fieldNames)
     tic;
     Name = fieldNames{ii};
     data = RData.(Name);
-    % determine the average sample rate of the data
-    fr = 1/round(mean(diff(data.device_timestamp/1000000)), 3);
-    num = ceil(fr/4); % 0.26 second moving average filter
-    coeff = ones(1, num)/num;
-    df = diff(filter(coeff, 1, data.Fx)); % filter the data
-    offset = 400; % find when the sensor makes contact with the finger
-    [pks, idx] = findpeaks(df(offset:end), 'NPeaks', 1, 'SortStr', 'descend');
-    idx=idx-fr/2;
+    if size(data, 1) > 25000
+        data = data(1:25000, :)
+    end
     % Zero the sensor out
-    Zfx = mean(data.Fx(1:idx));
-    Zfy = mean(data.Fy(1:idx));
-    Zfz = mean(data.Fz(1:idx));
-    Zmx = mean(data.Mx(1:idx));
-    Zmy = mean(data.My(1:idx));
-    Zmz = mean(data.Mz(1:idx));
+    Zfx = mean(data.Fx(1:2000));
+    Zfy = mean(data.Fy(1:2000));
+    Zfz = mean(data.Fz(1:2000));
+    Zmx = mean(data.Mx(1:2000));
+    Zmy = mean(data.My(1:2000));
+    Zmz = mean(data.Mz(1:2000));
     % assemble the structure
     data.Fx = abs(data.Fx - Zfx);
     data.Fy = abs(data.Fy - Zfy);
@@ -53,10 +45,19 @@ for ii = 1:numel(fieldNames)
     data.Mx = data.Mx - Zmx;
     data.My = data.My - Zmy;
     data.Mz = data.Mz - Zmz;
+    % determine the average sample rate of the data
+    fr = 1/round(mean(diff(data.device_timestamp/1000000)), 3);
+    %% filter and crop?
+    num = ceil(fr/4); % 0.26 second moving average filter
+    df = diff(movmean(data.Fy, num)); % filter the data
+    offset = 400; % find when the sensor makes contact with the finger
+    idx = find(df > 0.01, 1, 'first');
+    idx2 = find(df(idx-100:idx) < 0.001, 1, 'last');
+    idx = idx-100+idx2-2; idx2 = idx+200*70;
 
     rokubi.(Name) = data;
     clear x data
     fprintf('-> Done Unpacking Trial %d in %.2f s\n', ii, toc);
 end
 % save the assembled structure to a mat file
-save('Rokubi_Data.mat', 'rokubi')
+save('Rokubi_Data_60x75.mat', 'rokubi')
--- Grasp-Force-Testing/Data/unpackTeensyData.m
+++ Grasp-Force-Testing/Data/unpackTeensyData.m
@@ -13,16 +13,13 @@
 clear all; close all; clc;
 
 %% Import Teensy Data (.log files from PuTTY)
-TData.t1 = readtable('T1', 'FileType', 'text');
-TData.t2 = readtable('T2', 'FileType', 'text');
-TData.t3 = readtable('T3', 'FileType', 'text');
-TData.t4 = readtable('T4', 'FileType', 'text');
-TData.t5 = readtable('T5', 'FileType', 'text');
-TData.t6 = readtable('T6', 'FileType', 'text');
-TData.t7 = readtable('T7', 'FileType', 'text');
-TData.t8 = readtable('T8', 'FileType', 'text');
-TData.t9 = readtable('T9', 'FileType', 'text');
-TData.t10 = readtable('T10', 'FileType', 'text');
+TData.t1 = readtable('Grasp_Force_Testing_Rokubi\90x90\T1.log', 'FileType', 'text');
+TData.t2 = readtable('Grasp_Force_Testing_Rokubi\90x90\T2.log', 'FileType', 'text');
+TData.t3 = readtable('Grasp_Force_Testing_Rokubi\90x90\T3.log', 'FileType', 'text');
+TData.t4 = readtable('Grasp_Force_Testing_Rokubi\90x90\T4.log', 'FileType', 'text');
+TData.t5 = readtable('Grasp_Force_Testing_Rokubi\90x90\T5.log', 'FileType', 'text');
+TData.t6 = readtable('Grasp_Force_Testing_Rokubi\90x90\T6.log', 'FileType', 'text');
+
 
 fieldNames = fieldnames(TData);
 kk = 1;
@@ -32,37 +29,42 @@ for ii = 1:numel(fieldNames)
     data = TData.(Name);
     for jj = 1:size(data, 1)
         if jj == 1
-            x.time(jj, 1) = 0.03; % Relative time in seconds
-            x.t1(jj, 1) = data{1, 2}; % \theta_1 of the finger (rad)
-            x.t2(jj, 1) = data{2, 2}; % \theta_2 of the finger (rad)
-            x.t3(jj, 1) = data{3, 2}; % \theta_A/actuator angle (rad) 
-            x.p1(jj, 1) = data{4, 2}; % approximated p_1 (m)
-            x.p1(jj, 1) = data{5, 2}; % approximated p_2 (m)
-            x.F(jj, 1) = data{6, 2};  % approximated total enveloping force (N)
+            x.time(jj, 1) = 0.0; % Relative time in seconds
+            x.t1(jj, 1) = data{1, 1}; % \theta_1 of the finger (rad)
+            x.t2(jj, 1) = data{1, 2}; % \theta_2 of the finger (rad)
+            x.t3(jj, 1) = data{1, 3}; % \theta_3/actuator angle (rad) 
+            x.c(jj, 1) = data{1, 4};
+            % x.p1(jj, 1) = data{4, 2}; % approximated p_1 (m)
+            % x.p1(jj, 1) = data{5, 2}; % approximated p_2 (m)
+            x.F(jj, 1) = data{1, 7};  % approximated total enveloping force (N)
+            x.Ref(jj, 1) = data{1, 9};
             kk = kk+1;
         else
             try
-                str = cell2mat(data{jj, 1});
-                % search for the string 'Reporting' to collect the next
-                % frame
-                if strcmp(str(1:9), 'Reporting') == 1
-                    % the significant digits of time changes
-                    try 
-                        x.time(kk, 1) = str2num(str(end-9:end-3));
-                    catch
-                        try 
-                            x.time(kk, 1) = str2num(str(end-8:end-3));
-                        catch
-                            x.time(kk, 1) = str2num(str(end-7:end-3));
-                        end
-                    end
-                end
-                x.t1(kk, 1) = data{jj+1, 2};
-                x.t2(kk, 1) = data{jj+2, 2};
-                x.t3(kk, 1) = data{jj+3, 2};
-                x.p1(kk, 1) = data{jj+4, 2};
-                x.p2(kk, 1) = data{jj+5, 2};
-                x.F(kk, 1) = data{jj+6, 2};
+                % str = cell2mat(data{jj, 1});
+                % % search for the string 'Reporting' to collect the next
+                % % frame
+                % if strcmp(str(1:9), 'Reporting') == 1
+                %     % the significant digits of time changes
+                %     try 
+                %         x.time(kk, 1) = str2num(str(end-9:end-3));
+                %     catch
+                %         try 
+                %             x.time(kk, 1) = str2num(str(end-8:end-3));
+                %         catch
+                %             x.time(kk, 1) = str2num(str(end-7:end-3));
+                %         end
+                %     end
+                % end
+                x.time(kk, 1) = (jj-1)/10;
+                x.t1(kk, 1) = data{jj, 1};
+                x.t2(kk, 1) = data{jj, 2};
+                x.t3(kk, 1) = data{jj, 3};
+                x.c(kk, 1) = data{jj, 4};
+                % x.p1(kk, 1) = data{jj, 1};
+                % x.p2(kk, 1) = data{jj, 1};
+                x.F(kk, 1) = data{jj, 7};
+                x.Ref(kk, 1) = data{jj, 9};
                 kk = kk+1;
             end
         end
@@ -80,4 +82,14 @@ for ii = 1:numel(fieldNames)
     kk = 1;
 end
 % save the assembled structure to a mat file
-save('Finger_Data.mat', 'finger')
+save('Finger_Data_90x90.mat', 'finger')
+
+% ref.time = linspace(0, 110, 111)
+% ref.F = 5*ones(size(ref.time))
+% for ii = 1:11
+%     if ii < 7
+%         ref.F = ref.F + 5*heaviside(10*(ii-1));
+%     else
+%         ref.F = ref.F - 5*heaviside(10*(ii-1));
+%     end
+% end
